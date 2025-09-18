f5b38eb7f70731574e91cb824b17888061d34aa0
bekkhurramov524 <bekkhurramov.com>
2025-09-18 22:17:26 +0500

    masalam

---
+++ masalarr/index.js
@@ -0,0 +1,79 @@
+// const numbers = [10, 20, 30,40,50];
+// let number = numbers.slice(1,4);
+// console.log(number);
+//1
+// const harflar = ["a","b","c","d","e"]
+// let harf=harflar.slice(3,)
+// console.log(harf)
+//2
+// const numbers = [1, 2, 3,4,5,6];
+// let num =numbers.slice(0,3)
+// console.log(num)
+//3
+// const numbers = [1, 2, 3,4];
+// let num = numbers.splice(1, 1,5);
+// console.log(numbers);
+//1
+// const harflar = ["a","b","c","d"]
+// let harf=harflar.splice(1,2)
+// console.log(harflar)
+//2
+// const numbers= [10,20,30]
+// let num=numbers.splice(1,1,25)
+// console.log(numbers)
+//3
+// const numbers = [1,2,3,4,5,6,]
+// let num = numbers.fill(0)
+// console.log(num)
+// const numbers = [1, 2, 3, 4, 5]
+// let num =numbers.fill(99,2)
+// console.log(num)
+// const a = new Array(3).fill(null);
+// const b = new Array(3).fill(null);
+// const c = new Array(3).fill(null);
+// console.log(a); // [null, null, null]
+// console.log(b); // [null, null, null]
+// console.log(c); // [null, null, null]
+// const numbers =[4, 5, 6, 7]
+// let num = numbers.indexOf(6)
+// console.log(num)
+// const harflar =["x","y", "z"]
+// let harf = harflar.indexOf("a")
+// console.log(harf)
+
+
+// const numbers =[1, 2, 3, 2, 1]
+// let num = numbers.indexOf(2)
+// console.log(num)
+// const numbers = [1, 2, 3];
+// let num = numbers.toString();
+// console.log( num);
+
+function powerA3(x) {
+    return x ** 3; // yoki Math.pow(x, 3);
+}
+
+
+// let A = 2.5;
+// let B = -1.2;
+// let C = 3.14;
+// let D = 4;
+// let E = -7;
+//
+// console.log("A", powerA3(A));
+// console.log("B", powerA3(B));
+// console.log("C", powerA3(C));
+// console.log("D:", powerA3(D));
+// console.log("E:", powerA3(E));
+// function Power234(x) {
+//     return {
+//         kvadrat: x ** 2,
+//         kub: x ** 3,
+//         tortinchi: x ** 4
+//     };}
+// let A = 2.5;
+// let B = -1.2;
+// let C = 3.0;
+// console.log("A =", A, "→", Power234(A));
+// console.log("B =", B, "→", Power234(B));
+// console.log("C =", C, "→", Power234(C));
